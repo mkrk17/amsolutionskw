e487253a31e87627f2a3bcb9ab9ae034b6e64132
Ramakumar <ramakumar@Ramakumars-MacBook-Air.local>
2023-12-25 19:11:05 +0530

    logo and navbar changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function userScroll() {
-  const navbar = document.querySelector('.navbar');
-  const toTopBtn = document.querySelector('#to-top');
+  const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('navbar-sticky');
-      toTopBtn.classList.add('show');
+      navbar.classList.add("navbar-sticky", "navbar-shrink");
+      toTopBtn.classList.add("show");
     } else {
-      navbar.classList.remove('navbar-sticky');
-      toTopBtn.classList.remove('show');
+      navbar.classList.remove("navbar-sticky", "navbar-shrink");
+      toTopBtn.classList.remove("show");
     }
   });
 }
@@ -19,13 +19,13 @@ function scrollToTop() {
 }
 
 function incrementStats() {
-  const counters = document.querySelectorAll('.counter');
+  const counters = document.querySelectorAll(".counter");
 
   counters.forEach((counter) => {
     counter.innerText = 0;
 
     const updateCounter = () => {
-      const target = +counter.getAttribute('data-target');
+      const target = +counter.getAttribute("data-target");
       const c = +counter.innerText;
 
       const increment = target / 200;
@@ -43,6 +43,6 @@ function incrementStats() {
 }
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', userScroll);
-document.addEventListener('DOMContentLoaded', incrementStats);
-document.querySelector('#to-top').addEventListener('click', scrollToTop);
+document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", incrementStats);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
